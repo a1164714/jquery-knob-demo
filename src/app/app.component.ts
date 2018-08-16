3749936f63ad6eac6c3dfa0cba774109a50ffcae
LinZhanfei <zhanfei.lin@delgihtintl.com>
2018-08-16 09:39:56 +0800

    jquery-knob demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
+import 'jquery-knob';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    $(function() {
+      $(".dial").knob();
+    });
+  }
 }
